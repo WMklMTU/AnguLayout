6c2309ed1724f275459d116f999824a0020be221
Waldemar Mikluszka <43411268+WMklMTU@users.noreply.github.com>
2021-08-26 17:10:12 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
-
 @NgModule({
   declarations: [AppComponent, NavigationComponent],
   imports: [
