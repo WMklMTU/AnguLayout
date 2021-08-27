11bdf16d54e91c92e7521fdf18879fdd23bbb554
Waldemar Mikluszka <waldemar.mikluszka@mtupolska.com>
2021-08-27 17:13:58 +0200

    first routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { HomeComponent } from './home/home.component';
+import { ChartComponent } from './chart/chart.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent}
+  {path:'', component: HomeComponent},
+  {path:'chart', component: ChartComponent}
 ];
 
 @NgModule({
